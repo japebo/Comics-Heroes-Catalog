de8178000bda4e5a5f8afa4cf1018d47977e8d8e
Javier Alejandro Pérez Borges <javiborges95@gmail.com>
2022-01-11 18:03:05 -0500

    Added a context and a reducer for managing user authentication. Public and private routes were also protected. Made a workaround to remember the last page visited by the user

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types';
 
 export const LoginScreen = ({history}) => {
+    
+    const { dispatch } = useContext(AuthContext);        
 
-    const handleClick = () => {
+    const handleLogin = () => {
         // history.push('/marvel');
-        history.replace('/marvel'); //replace in the browser's history /login for /marvel, thus if we go back we won't go to /login but to the previous visited url 
+
+        const lastPath = localStorage.getItem('lastPath') || '/marvel';
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Javier'
+            }
+        });
+
+        history.replace( lastPath ); //replace in the browser's history /login for /marvel, thus if we go back we won't go to /login but to the previous visited url 
     }
 
     return (
@@ -13,7 +27,7 @@ export const LoginScreen = ({history}) => {
             <hr/>
             <button
                 className='btn btn-primary'
-                onClick={ handleClick }
+                onClick={ handleLogin }
             >
                 Login
             </button>
